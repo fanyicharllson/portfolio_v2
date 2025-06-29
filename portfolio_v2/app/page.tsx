22d46a6cf38356733f443a32b5f7b214d008ad37
fanyicharllson <fanyicharllson1@gmail.com>
2025-06-28 18:03:02 -0700

    Initail Project setup

---
+++ portfolio_v2/app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+    <div className='flex items-center text-red-600 text-4xl font-bold justify-center'>Portfolio</div>
+  )
+}
